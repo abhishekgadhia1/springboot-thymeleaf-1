686f8be074924bbeca37bfcf9c4548c594983b4c
abhishekgadhia1 <abhishek.gadhia@gmail.com>
2023-02-16 23:42:59 +0530

    first

---
+++ src/main/java/com/practice/SpringBootThymeleafOne/SpringBootThymeleafTwoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.SpringBootThymeleafOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/SpringBootThymeleafOne/SpringBootThymeleafTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.SpringBootThymeleafOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
